cf337449e332e60816148d233a24a5850e82bcbc
muhammadhamza7057 <muhammadhamza7057@gmail.com>
2026-05-19 02:04:46 +0500

    Allow PATCH in production CORS config

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ if (!fs.existsSync(uploadsDir)) {
 const corsOptions = {
   origin: ['https://fundbridge-ten.vercel.app', 'http://localhost:3000', 'http://localhost:5173'],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
 };
 
